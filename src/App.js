2cb51a5f2682c2bf510f0f4f1cd9658eda15b785
vidann1 <vidans233@yahoo.com>
2018-09-26 17:27:55 +0200

    added toggle button

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import "./styles/App.css";
 import ImgSlider from "./components/ImgSlider";
 
 class App extends Component {
+  state = {
+    visible: true
+  };
   render() {
+    const btnText = this.state.visible ? "hide" : "show";
     return (
       <div className="App">
         <header className="App-header">
           <h1>image slider</h1>
         </header>
 
-        <ImgSlider />
+        {this.state.visible ? <ImgSlider /> : <small>hidden</small>}
+        <button
+          onClick={() => {
+            this.setState({
+              visible: !this.state.visible
+            });
+          }}
+        >
+          {btnText}
+        </button>
       </div>
     );
   }
